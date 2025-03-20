85811af442ad9f7bb351f56188638ab05fe49164
Tio Mahlendra <septionomahlendra2004@gmail.com>
2025-03-20 10:05:46 +0800

    lah sudah

--- js/script.js
+++ js/script.js
@@ -130,17 +130,16 @@ gsap.from(".detail_blog",{
 
 window.addEventListener('scroll', (e)=>{
     const nav = document.querySelector('.header');
-    if (window.pageYOffset >=30){
+    if (window.pageYOffset >= 30){
         nav.classList.add("shadow-header")
-    }else {
+    }else{
         nav.classList.remove("shadow-header")
     }
-}
-)
+})
 
-// load all type rumah 
+// LOAD ALL TYPE RUMAH
 
-const jsonData={
+const jsonData = {
     "rumah": [
         {
             "tipe": "A",
@@ -151,7 +150,7 @@ const jsonData={
             "kamar_mandi": 2,
             "kamar_tidur": 3,
             "deskripsi": "Casa Verde, rumah dengan desain modern dan luas tanah yang cukup untuk kehidupan keluarga. Dengan dua kamar mandi dan tiga kamar tidur, rumah ini memberikan kenyamanan dan kehangatan bagi keluarga Anda.",
-            "harga":"1M - 2M",
+            "harga":"700Jt-an",
             "carport":"1",
             "denah_rumah":"./img/denah_rumah.jpg"
         },
@@ -193,63 +192,62 @@ const jsonData={
             "harga":"950Jt-an",
             "carport":"1",
             "denah_rumah":"./img/denah_rumah.jpg"
-        },
+        }
     ]
 }
 
-
-// function dynamic create html for each house 
+// function dynamic create html for each house
 function createHouseCard(house, index, isMobileView){
-    const isOod = index % 2 === 1;
+    const isOdd = index % 2 === 1;
 
     const penjelasan = `
-    <div class="col-md-6 mt-4 mb-4">
-    <h2>${house.nama}</h2>
-    <p>${house.deskripsi}</p>
-    <div class="row">
-        <div class="col-md-5 mb-3">
-            <div class="card justify-content-center align-self-center align-item-center p-3 tipe_rumah_item">
-            <i class="fa fa-briefcase"></i>
-            <h3>${house.luas_bangunan}</h3>
-            <p>Luas Bangunan </p>   
+      <div class="col-md-6 mt-4 mb-4">
+        <h2>${house.nama}</h2>
+        <p>${house.deskripsi}</p>
+        <div class="row">
+          <div class="col-md-5 mb-3">
+            <div class="card justify-content-center align-self-center  p-3 tipe_rumah_item">
+              <i class="fa fa-briefcase"></i>
+              <h3>${house.luas_bangunan}</h3>
+              <p>Luas Bangunan</p>
             </div>
-        </div>
-         <div class="col-md-5 mb-3">
-            <div class="card justify-content-center align-self-center align-item-center p-3 tipe_rumah_item">
-            <i class="fa fa-briefcase"></i>
-            <h3>${house.luas_tanah}</h3>
-            <p>Luas Tanah </p>   
+          </div>
+          <div class="col-md-5 mb-3">
+            <div class="card justify-content-center align-self-center  p-3 tipe_rumah_item">
+              <i class="fa fa-briefcase"></i>
+              <h3>${house.luas_tanah}</h3>
+              <p>Luas Tanah</p>
             </div>
-        </div>
-         <div class="col-md-5 mb-3">
-            <div class="card justify-content-center align-self-center align-item-center p-3 tipe_rumah_item">
-            <i class="fa fa-shower"></i>
-            <h3>${house.kamar_mandi}</h3>
-            <p>Kamar Mandi </p>   
+          </div>
+          <div class="col-md-5 mb-3">
+            <div class="card justify-content-center align-self-center  p-3 tipe_rumah_item">
+              <i class="fa fa-shower"></i>
+              <h3>${house.kamar_mandi}</h3>
+              <p>Kamar Mandi</p>
             </div>
-        </div>
-         <div class="col-md-5 mb-3">
-            <div class="card justify-content-center align-self-center align-item-center p-3 tipe_rumah_item">
+          </div>
+          <div class="col-md-5 mb-3">
+          <div class="card justify-content-center align-self-center  p-3 tipe_rumah_item">
             <i class="fa fa-bed"></i>
             <h3>${house.kamar_tidur}</h3>
-            <p>Kamar Tidur </p>   
-            </div>
+            <p>Kamar Tidur</p>
+          </div>
         </div>
-    </div>
-    <button class="btn_home view_more_btn mt-1">
-    Selengkapnya
-    </button>
-    </div>
-    ` 
+        </div>
+        <a href="./detail_rumah.html?tipe_rumah=${house.tipe}" class="btn_home view_more_btn mt-1" style="text-decoration:none;">
+            Selengkapnya
+        </a>
+      </div>
+    `;
 
-    const  gambar_rumah = `
+    const gambar_rumah = `
         <div class="col-md-6 mt-4 mb-4">
             <img src="${house.gambar_rumah}" alt="${house.nama}" class="img-fluid" />
         </div>
     `;
 
     var html = ``
-    if (isOod) {
+    if (isOdd) {
         html = gambar_rumah + penjelasan;
     }else{
         html = penjelasan + gambar_rumah;
@@ -257,10 +255,24 @@ function createHouseCard(house, index, isMobileView){
 
     // handle mobile view 
     if (isMobileView){
-        html= gambar_rumah+penjelasan;
+        html = gambar_rumah+penjelasan;
     }
 
     return html;
+}
+
+function createTipeLain(house){
+    const item = `
+        <div class="col-md-6 mt-4 mb-4">
+            <img src="${house.gambar_rumah}" class="w-100" style="max-height:200px; object-fit:cover; object-position:bottom">
+            <br><br>
+            <a href="./detail_rumah.html?tipe_rumah=${house.tipe}">
+              <h3 class="text-center">Rumah ${house.nama}</h3>
+            </a>
+        </div>
+    `
+
+    return item;
 }
 
 // load data into html (web view)
@@ -280,4 +292,196 @@ if (rumahContainerMobile != null){
         const houseCard = createHouseCard(house, index, true);
         rumahContainerMobile.innerHTML += houseCard;
     })
+}
+
+// load data tipe lain
+const lihatTipeLain = document.getElementById('lihatTipeLain')
+if (lihatTipeLain != null){
+    jsonData.rumah.forEach((house) => {
+        const tipeCard = createTipeLain(house);
+        lihatTipeLain.innerHTML += tipeCard;
+    });
+}
+
+// LOAD DETAIL RUMAH --------------------------------
+
+function filterRumahByTipe(tipe){
+    const filteredRumah = jsonData.rumah.find(rumah => rumah.tipe == tipe);
+    return filteredRumah || null;
+}
+
+var queryString = window.location.search;
+
+var searchParams = new URLSearchParams(queryString);
+
+var tipeRumahValue = searchParams.get('tipe_rumah');
+
+// load data to view html
+if (tipeRumahValue != null && tipeRumahValue != ""){
+    const filteredRumah = filterRumahByTipe(tipeRumahValue.toUpperCase())
+
+    if (filteredRumah == null){
+        console.log("rumah not found");
+    }else{
+        document.getElementById("title-nama-rumah").innerText = 'Rumah '+ filteredRumah.nama;
+        document.getElementById("gambar-rumah").src = filteredRumah.gambar_rumah;
+        document.getElementById("nama-rumah").innerText = filteredRumah.nama;
+        document.getElementById("deskripsi-rumah").innerText = filteredRumah.deskripsi;
+        document.getElementById("harga-rumah").innerText = filteredRumah.harga;
+        document.getElementById("luas-bangunan-rumah").innerText = filteredRumah.luas_bangunan+ 'm²';
+        document.getElementById("luas-tanah-rumah").innerText = filteredRumah.luas_tanah+ 'm²';
+        document.getElementById("kamar-mandi-rumah").innerText = filteredRumah.kamar_mandi;
+        document.getElementById("kamar-tidur-rumah").innerText = filteredRumah.kamar_tidur;
+        document.getElementById("carport-rumah").innerText = filteredRumah.carport;
+        document.getElementById("denah-rumah").src = filteredRumah.denah_rumah;
+    }
+}   
+
+// For blog load data 
+
+const blogData = [
+    {
+        id:1,
+        title:"Perumahan Dengan Hunian Nyaman dan Modern: Casa Verde di Myskill Residence",
+        date:"17 Maret 2025, 09:00 AM",
+        description: "Dalam dunia properti, terutama di industri perumahan, kebutuhan akan hunian yang nyaman dan modern semakin menjadi prioritas bagi masyarakat urban. Salah satu perumahan yang menawarkan konsep tersebut adalah Myskill Residence dengan tipe hunian bernama Casa Verde...",
+        detail_blog:"./content/blog-1.html",
+        image_blog:"./img/house-1.png"
+    },
+    {
+        id:2,
+        title: "Menikmati Kemewahan Hidup di Sky Villa : Rumah Hunian Modern dengan Pemandangan Mengagumkan",
+        date:"22 Januari 2025, 08:00 AM",
+        description: "Selamat datang di Sky Villa, rumah hunian modern yang menghadirkan kemewahan dan kenyamanan di tengah-tengah pemandangan yang menakjubkan. Ini bukan sekadar tempat tinggal, tetapi sebuah pengalaman hidup bergaya dan bersantai di atas langit biru..",
+        detail_blog:"./content/blog-2.html",
+        image_blog:"./img/house-2.png"
+    },
+]
+
+function generateBlogHTML(blogPost){
+    return `
+    <div class="card mb-4">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="${blogPost.image_blog}" width="100%" height="100%" />
+            </div>
+            <div class="col-md-8 p-4">
+                <div>
+                    <h5>${blogPost.title}</h5>
+                    <div class="blog-date d-flex mt-1">
+                        <i class="fa fa-calendar me-2 mt-1"></i>
+                        <p>${blogPost.date}</p>
+                </div>
+                <p class="description-blog mt-1">${blogPost.description}</p>
+                <a href="./detail_blog.html?id=${blogPost.id}" class="btn btn-sm btn-primary btn-readmore">
+                    <i class="fa fa-book me-1"></i>
+                    read more
+                </a>
+            </div>
+    `;
+}
+
+
+function generateLatestBlogHTML(blogPost) {
+    return `
+        <a href="./detail_blog.html?id=${blogPost.id}" class="latest-blog-items">
+            <p>${blogPost.title}</p>
+            <div class="blog-date d-flex mt-1">
+                <i class="fa fa-calendar me-2 mt-1"></i>
+                <p>${blogPost.date}</p>
+            </div>
+            <hr>
+        </a>
+    `;
+}
+
+function renderBlogPosts(filteredBlogData, isFiltered){
+    const blogContainer = document.getElementById('blogContainer');
+    const latestBlogContent = document.getElementById('latestBlogContent');
+
+    // render latest blog
+    if (latestBlogContent != null){
+        //clear previous content
+        latestBlogContent.innerHTML = '';
+
+        const lastTwoBlogPosts = blogData.slice(-2);
+        lastTwoBlogPosts.forEach(blogPost => {
+            latestBlogContent.innerHTML += generateLatestBlogHTML(blogPost)
+        });
+    }
+
+    if (blogContainer == null){
+        return // skipped
+    }
+
+    // clear previous content
+    blogContainer.innerHTML = '';
+
+    // check filtering data
+    if (isFiltered){
+        if (filteredBlogData.length == 0){
+            const emptyStateDiv = document.createElement('div');
+            emptyStateDiv.textContent = "No blog posts available";
+            blogContainer.appendChild(emptyStateDiv);
+        }
+        filteredBlogData.forEach(blogPost => {
+            blogContainer.innerHTML = generateBlogHTML(blogPost)
+        });
+    }else{
+        blogData.forEach(blogPost => {
+            blogContainer.innerHTML += generateBlogHTML(blogPost)
+        });
+    }
+}
+
+function searchBlog(input){
+    const searchTerm = input.value.toLowerCase();
+    const filteredBlogData = []
+    blogData.forEach(blogPost => {
+        if (blogPost.title.toLowerCase().includes(searchTerm)){
+            filteredBlogData.push(blogPost);
+        }
+    })
+    console.log("blogData",blogData)
+    console.log("searchTerm",searchTerm)
+    console.log("filteredBlogData",filteredBlogData)
+
+    if (searchTerm != null && searchTerm != "" && searchTerm != undefined){
+        renderBlogPosts(filteredBlogData, true)
+    }else{
+        renderBlogPosts([], false)
+    }
+}
+
+// render blog posts when page loads
+window.onload = renderBlogPosts([],false);
+
+// render value parms query id 
+var idBlogvalue = searchParams.get('id');
+
+if (idBlogvalue != null && idBlogvalue != ""){
+    const filteredBlogData = getBlogById(parseInt(idBlogValue,10));
+    if (filteredBlogData != null && filteredBlogData != undefined){
+        document.getElementById ("cardDetailImg").src = filteredBlogData.image_blog;
+        document.getElementById("tgl-blog").innerText = filteredBlogData.date;
+        document.getElementById("title-blog").innerText = filteredBlogData.title;
+        loadDetailBlog(filteredBlogData.detail_blog)
+    }else{
+        console.log("errror on data parameter:" + idBlogvalue)
+    }
+}
+
+
+
+function getBlogById(blogId){
+    return blogData.filter(blog => blog.id === blogId)[0];
+}
+
+function loadDetailBlog(detailBlogURL){
+    const cardDetailBlog = document.getElementById("cardDetailBlog");
+    if (cardDetailBlog == null || cardDetailBlog == undefined){
+        console.log("no element on car has id   ");
+        return  
+    }
+    document.getElementById("cardDetailBlog").src = detailBlogURL;
 }
